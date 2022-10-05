679484d03952b5714b877f22e401e76bc699ffbc
CNPolaris <cntianxin@outlook.com>
2022-10-05 16:56:18 +0800

    基础rrt算法实现的部分优化

--- rrt/rrt.py
+++ rrt/rrt.py
@@ -3,9 +3,11 @@
 """
 
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
 import random
 import math
 import copy
+import numpy as np
 
 show = True
 
@@ -36,13 +38,17 @@ class RRT(object):
         """
         self.start = Node(start[0], start[1])
         self.goal = Node(goal[0], goal[1])
+        self.safe = Node(20, 20)
+        self.safe.parent = 0
         self.min_rand = rand_area[0]
         self.max_rand = rand_area[1]
-        self.expandDis = 5  # 探索步长
+        self.step = 10  # 探索步长
+        self.distance = 5
         self.goalSampleRate = 0.05  # 选择终点的概率
         self.maxIter = 500
         self.obstacle_list = obstacle_list
-        self.node_list = [self.start]
+        self.node_list = [self.start, self.safe]
+        self.angle = 60
 
     def random_node(self):
         """
@@ -56,63 +62,112 @@ class RRT(object):
 
     @staticmethod
     def get_nearest_list_index(node_list, rnd):
+        """
+        在节点列表中找到距离目标节点中最近的点
+        :param node_list: 节点列表 T1 or obstacle_list
+        :param rnd: 目标节点
+        :return: 最近节点的位置
+        """
         d_list = [(node.x - rnd[0]) ** 2 + (node.y - rnd[1]) ** 2 for node in node_list]
         min_index = d_list.index(min(d_list))
         return min_index
 
-    @staticmethod
-    def collision_check(new_node, obstacle_list):
+    def get_nearest_obstacle_index(self, point):
+        """
+        找到距离point最近的障碍物
+        :param point: 节点
+        :return: 最近的障碍物
+        """
+        d_list = [(math.sqrt((point.x - x) ** 2 + (point.y - y) ** 2)) - r for (x, y, r) in self.obstacle_list]
+        min_index = d_list.index(min(d_list))
+        return min_index
+
+    def collision_check(self, new_node, obstacle_list):
         """
         检查新的节点是否会碰撞
-        :param new_node:
-        :param obstacle_list:
+        :param new_node:  实新节点
+        :param obstacle_list: 障碍物列表
         :return:
         """
-        a = 1
-        for (ox, oy, size) in obstacle_list:
+        flag = 1
+        for (ox, oy, radius) in obstacle_list:
             dx = ox - new_node.x
             dy = oy - new_node.y
             d = math.sqrt(dx * dx + dy * dy)
-            if d <= size:
-                a = 0
-        return a
 
-    def planning(self):
+            parent_point = self.node_list[new_node.parent]
+            vector_o = np.array([ox, oy])
+            vector_p = np.array([parent_point.x, parent_point.y])
+            vector_n = np.array([new_node.x, new_node.y])
+            d_p_n = np.abs(np.cross(vector_p - vector_o, vector_n - vector_o)) / np.linalg.norm(vector_p - vector_n)
+
+            if d <= radius or d_p_n < radius:
+                flag = 0
+                return flag
+        return flag
+
+    def dynamic_step(self, n_point, a_point):
         """
-        路径计划
+        计算动态步长
+        :param n_point: 父节点
+        :param a_point: 虚新节点
+        :return: Sf 动态步长
+        """
+
+        tan = math.atan2(a_point.y - n_point.y, a_point.x - n_point.x)
+        a_point.x += math.cos(tan) * (self.distance + self.step) / 2
+        a_point.y += math.sin(tan) * (self.distance + self.step) / 2
+        # 距离最近的障碍物
+        obstacle_min = self.obstacle_list[self.get_nearest_obstacle_index(a_point)]
+        # 虚拟节点a_point至障碍物边缘的距离l_a
+        # l_a = math.sqrt((a_point.x - obstacle_min[0]) ** 2 + (a_point.y - obstacle_min[1]) ** 2) - obstacle_min[2]
+        # 生长点n_point至障碍物边缘的距离l_n
+        l_n = math.sqrt(np.square(n_point.x - obstacle_min[0]) + np.square(n_point.y - obstacle_min[1])) - obstacle_min[
+            2]
+
+        dynamic = self.step / (1 + (self.step / self.distance - 1) * math.exp(-3 * l_n / self.step))
+        return dynamic
+
+    def angle_check(self, new_point, parent_point, ancestor_point):
+        """
+        转角约束
+        :param new_point: 新节点 w
+        :param parent_point: n节点(新节点的父级节点)
+        :param ancestor_point: f祖先节点
         :return:
         """
-        while True:
-            if random.random() > self.goalSampleRate:
-                rnd = self.random_node()
-            else:
-                rnd = [self.goal.x, self.goal.y]
+        vector_p_n = np.array([new_point.x - parent_point.x, new_point.y - parent_point.y])
+        vector_a_p = np.array([parent_point.x - ancestor_point.x, parent_point.y - ancestor_point.y])
+        angle = get_angle(vector_a_p, vector_p_n)
+        if angle <= self.angle:
+            return True
+        else:
+            return False
 
-            min_index = self.get_nearest_list_index(self.node_list, rnd)
+    def coordinate(self, rnd):
+        """
+        实际坐标计算
+        :param rnd: 虚新节点
+        :return: 实际新节点
+        """
+        # 在搜索树中找到距离rnd最近的节点
+        min_index = self.get_nearest_list_index(self.node_list, rnd)
+        nearest_point = self.node_list[min_index]
 
-            nearest_node = self.node_list[min_index]
+        # 按照原始步长计算坐标
+        theta = math.atan2(rnd[1] - nearest_point.y, rnd[0] - nearest_point.x)
+        new_point = copy.deepcopy(nearest_point)
+        new_point.x += math.cos(theta) * self.step
+        new_point.y += math.sin(theta) * self.step
+        # 使用动态步长策略计算实际坐标
+        actual_step = self.dynamic_step(nearest_point, new_point)
+        new_point.x = nearest_point.x + math.cos(theta) * actual_step
+        new_point.y = nearest_point.y + math.sin(theta) * actual_step
+        new_point.parent = min_index
 
-            theta = math.atan2(rnd[1] - nearest_node.y, rnd[0] - nearest_node.x)
-            new_node = copy.deepcopy(nearest_node)
-            new_node.x += self.expandDis * math.cos(theta)
-            new_node.y += self.expandDis * math.sin(theta)
-            new_node.parent = min_index
+        return new_point
 
-            if not self.collision_check(new_node, self.obstacle_list):
-                continue
-
-            self.node_list.append(new_node)
-
-            dx = new_node.x - self.goal.x
-            dy = new_node.y - self.goal.y
-            d = math.sqrt(dx * dx + dy * dy)
-            if d <= self.expandDis:
-                print("goal")
-                break
-
-            # if True:
-            #     self.draw_graph(rnd)
-
+    def generate_path_list(self):
         path = [[self.goal.x, self.goal.y]]
         last_index = len(self.node_list) - 1
         while self.node_list[last_index].parent is not None:
@@ -120,11 +175,49 @@ class RRT(object):
             path.append([node.x, node.y])
             last_index = node.parent
         path.append([self.start.x, self.start.y])
-
         return path
 
+    def planning(self):
+        """
+        路径计划
+        :return:
+        """
+        while True:
+            # 生成随机点
+            if random.random() > self.goalSampleRate:
+                rnd = self.random_node()
+            else:
+                rnd = [self.goal.x, self.goal.y]
+            # 计算实新节点
+            new_node = self.coordinate(rnd)
+            # 碰撞检测
+            if not self.collision_check(new_node, self.obstacle_list):
+                continue
+            # 父节点
+            n_point = self.node_list[new_node.parent]
+            if n_point.parent is None:
+                continue
+            # 祖先节点
+            f_point = self.node_list[n_point.parent]
+            # 转角约束
+            if not self.angle_check(new_node, n_point, f_point):
+                continue
+
+            self.node_list.append(new_node)
+            # 接近判断
+            dx = new_node.x - self.goal.x
+            dy = new_node.y - self.goal.y
+            d = math.sqrt(dx * dx + dy * dy)
+            if self.distance <= d <= self.step:
+                print("goal")
+                break
+
+            # if True:
+            #     self.draw_graph(rnd)
+        # 路径回溯
+        return self.generate_path_list()
+
     def draw_graph(self, rnd=None):
-        print("find")
         plt.clf()
         if rnd is not None:
             plt.plot(rnd[0], rnd[1], "^g")
@@ -151,6 +244,14 @@ class RRT(object):
         """
         plt.clf()
 
+        x_major_location = MultipleLocator(10)
+        # y轴刻度区间
+        y_major_location = MultipleLocator(10)
+        # 坐标轴实例
+        ax = plt.gca()
+        ax.xaxis.set_major_locator(x_major_location)
+        ax.yaxis.set_major_locator(y_major_location)
+
         for node in self.node_list:
             if node.parent is not None:
                 plt.plot([node.x, self.node_list[node.parent].x],
@@ -176,6 +277,26 @@ class RRT(object):
             plt.plot(px, py, '-r')
         plt.grid(True)
         plt.show()
+
+
+def get_angle(a, b):
+    """
+    向量夹角计算
+    :param a: 向量1
+    :param b: 向量2
+    :return: 角度值
+    """
+    a_norm = np.sqrt(np.sum(a * a))
+    b_norm = np.sqrt(np.sum(b * b))
+    cos_value = float(np.dot(a, b) / (a_norm * b_norm))
+    eps = 1e-6
+    if 1.0 < cos_value < 1.0 + eps:
+        cos_value = 1.0
+    elif -1.0 - eps < cos_value < -1.0:
+        cos_value = -1.0
+    arc_value = np.arccos(cos_value)
+    angle_value = arc_value * (180 / np.pi)
+    return angle_value
 
 
 def main():
